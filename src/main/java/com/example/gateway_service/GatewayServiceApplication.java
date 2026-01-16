054804c4746f5f6c1ceac9b44c5f2bafc071ee68
201912025 <michi2012@naver.com>
2026-01-16 13:07:41 +0900

    feat: 스레드 전환 구간에서 확실한 전파 위해 Hooks 설정 명시적으로 추가

--- src/main/java/com/example/gateway_service/GatewayServiceApplication.java
+++ src/main/java/com/example/gateway_service/GatewayServiceApplication.java
@@ -2,11 +2,14 @@ package com.example.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
+		Hooks.enableAutomaticContextPropagation();
+
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
 
